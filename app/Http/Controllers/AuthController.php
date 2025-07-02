c303ae46ad0e2d621780514e0500ecb6fe16e558
banu1312 <radityabanu1312@gmail.com>
2025-07-03 02:23:00 +0700

    0.0.2 create Feature Auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function auth(Request $request){
+        try{
+            $validate = $request->validate(
+                [
+                    "username" => "required|string|exists:users,username",
+                    "password" => "required|string"
+                ]
+                );
+            $username = $validate['username'];
+            $password = $validate['password'];
+
+            $user = User::firstWhere('username',$username);
+
+            if(!Hash::check($password,$user->password)){
+                return response([
+                    "msg" => "The email or password entered is incorrect."
+                ],Response::HTTP_NOT_FOUND);
+            }
+
+            $token = $user->createToken("auth")->plainTextToken;
+            // $setting = Setting::get();
+            $data = [
+                "user" => $user,
+                "token" => $token
+            ];
+            return response()->json([
+                "message" => "Success",
+                "data" => $data
+            ],Response::HTTP_OK);
+            
+        }catch(\Exception $e){
+            if ($e instanceof ValidationException) {
+                return response()->json(["message" => "Failed", "error" => $e->errors()], Response::HTTP_BAD_REQUEST);
+            }
+            return response()->json(["message" => "Failed", "error" => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $user = User::find(auth("sanctum")->user()->id);
+        $user->tokens()->delete();
+    }
+
+    public function store(Request $request){
+        try {
+            $validate = $request->validate([
+                "fullname"=>"required|string",
+                "username"=>"required|string|unique:users,username",
+                "password"=>"required|string",
+                "email"=>"required|email",
+                "role"=>"required|string|in:'SuperAdmin','Admin','User','Guest'",
+            ]);
+            $data = User::query()->create($validate);
+
+            return response()->json(["message" => "Success", "data" => $data]);
+        } catch (\Exception $ex) {
+            if ($ex instanceof ValidationException) {
+                return response()->json(["message" => "Failed", "error" => $ex->errors()], Response::HTTP_BAD_REQUEST);
+            }
+            return response()->json(["message" => "Failed", "error" => $ex->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+    public function update(Request $request, User $user)
+    {
+        try {
+            $validated = $request->validate([
+                'fullname' => ['string'],
+                'username' => ['string', Rule::unique('users', 'username')->ignore($user->id)],
+                'email'    => ['email', Rule::unique('users', 'email')->ignore($user->id)],
+                'password' => ['string'],
+            ]);
+    
+            // Enkripsi password jika diisi
+            if (isset($validated['password'])) {
+                $validated['password'] = bcrypt($validated['password']);
+            }
+    
+            $user->update($validated);
+    
+            return response()->json(["message" => "Success", "data" => $user]);
+        } catch (ValidationException $ex) {
+            return response()->json([
+                "message" => "Validation Failed",
+                "error" => $ex->errors()
+            ], Response::HTTP_BAD_REQUEST);
+        } catch (\Exception $ex) {
+            return response()->json([
+                "message" => "Failed",
+                "error" => $ex->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DailyQuestController;
 
-Route::apiResource('daily-quests', DailyQuestController::class);
+Route::post("login", [AuthController::class, "auth"]);
+
+Route::middleware("auth:sacntum")->group(function (){
+    Route::post("logout", [AuthController::class, "logout"]);
+    Route::post("register", [AuthController::class, "create"]);
+    Route::post("update", [AuthController::class, "update"]);
+});
